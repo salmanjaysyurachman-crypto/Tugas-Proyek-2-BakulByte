257ae07289a11a154847e2875c911bc88810b4d8
HASYIM <muhammadhasyimmaruf@smkitihsanulfikri.sch.id>
2026-04-30 10:07:31 +0700

    nambah lagi 1

--- Sistem Koperasi BuHa/Src/logger.py
+++ Sistem Koperasi BuHa/Src/logger.py
@@ -4,3 +4,8 @@ import os
 from logging.handlers import RotatingFileHandler
 from datetime import datetime
 from functools import wraps
+
+LOG_FILE        = "error_log.txt"
+MAX_BYTES       = 5 * 1024 * 1024   # 5 MB sebelum file dirotasi
+BACKUP_COUNT    = 3                  # simpan hingga 3 file lama
+ADMIN_ID        = os.getenv("ADMIN_ID")
